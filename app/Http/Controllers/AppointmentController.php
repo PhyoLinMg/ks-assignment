54a54d454149dfe50f8b36ee1b11cd1f53ef3a93
Lin Maung <gangsterplm@gmail.com>
2023-10-25 22:37:08 +0800

    making controller to create apis

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    //
+    public function getAppointments(){
+
+    }
+
+    public function getSpecificAppointment($id){
+
+    }
+
+    public function createAppointment(Request $request){
+
+    }
+
+    public function cancelAppointment(Request $request){
+
+    }
+}
