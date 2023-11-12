2b38a7d1a1d06b8a5b9dd99390f019f9249a2157
Lin Maung <gangsterplm@gmail.com>
2023-11-12 16:54:51 +0800

    push

--- app/Http/Resources/SpecializationResource.php
+++ app/Http/Resources/SpecializationResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DoctorResource extends JsonResource
+class SpecializationResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,11 +15,9 @@ class DoctorResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=> $this->id,
-            'name'=> $this->name,
-            'degree'=> $this->degree,
-            'biography'=> $this->biography,
-            'specializations'=> SpecializationResource::collection($this->specializations),
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'description'=>$this->description,
         ];
     }
 }
